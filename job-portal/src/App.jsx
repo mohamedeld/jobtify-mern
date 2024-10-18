54766e646e3b212042ac36c746d99b353014c333
mohamedeld <mohamed.azoz20010@gmail.com>
2024-10-18 15:27:27 +0300

    add landing page

--- job-portal/src/App.jsx
+++ job-portal/src/App.jsx
@@ -1,10 +1,30 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import HomeLayout from "./pages/HomeLayout";
+import {Landing,HomeLayout,Register,Login,DashboardLayout,Error} from "./pages"
 const router = createBrowserRouter([
   {
     path:'/',
-    element:<HomeLayout/>
-  }
+    element:<HomeLayout/>,
+    errorElement:<Error/>,
+    children: [
+      {
+        index:true,
+        element:<Landing/>
+      },
+      {
+        path:'register',
+        element:<Register/>
+      },
+      {
+        path:'login',
+        element:<Login/>
+      },
+      {
+        path:'dashboard',
+        element:<DashboardLayout/>
+      },
+    ]
+  },
+  
 ])
 
 const App = () => {
--- job-portal/src/pages/Landing.jsx
+++ job-portal/src/pages/Landing.jsx
@@ -1,7 +1,23 @@
-
+import Wrapper from "../assets/wrappers/LandingPage"
+import logo from "../assets/imgs/logo.svg";
+import main from "../assets/imgs/main.svg"
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify logo" className="logo"/>
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>job <span>tracking</span> app</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatum earum quibusdam facere temporibus dolore perferendis hic aliquam quaerat laborum deleniti exercitationem quae explicabo, mollitia nostrum rerum nobis aperiam cum.</p>
+          <Link to="/register" className="btn register-link">Register</Link>
+          <Link to="/login" className="btn ">Login / Demo User</Link>
+        </div>
+        <img src={main} alt="main home page image" className="img main-img"/>
+      </div>
+    </Wrapper>
   )
 }
 
